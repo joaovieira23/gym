0cadca39e4318113cf3446cdfddad3eb37285092
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-08 17:38:25 -0300

    feat: add message from empty list

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,4 +1,4 @@
-import { Heading, VStack, SectionList } from 'native-base';
+import { Heading, VStack, SectionList, Text } from 'native-base';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 import { useState } from 'react';
@@ -32,6 +32,16 @@ export function History() {
                     </Heading>
                 )}
                 px={8}
+                contentContainerStyle={exercises.length === 0 && {
+                    flex: 1,
+                    justifyContent: 'center'
+                }}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign='center'>
+                        Não há exercícios registrados ainda. {'\n'}
+                        Vamos fazer exercícios hoje?
+                    </Text>
+                )}
             />
         </VStack>
     )
