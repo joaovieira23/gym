412d8d0e732dff9fac547d17a6199375603387f2
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-08 16:00:30 -0300

    feat: header exercises component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,38 @@
+import { Heading, HStack, Text, VStack, Icon } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
+
+export function HomeHeader() {
+    return (
+        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems='center'>
+            <UserPhoto 
+                source={{
+                    uri: 'https://github.com/joaovieira23.png'
+                }}
+                alt="Imagem do usuário"
+                size={16}
+                mr={4}
+            />
+            <VStack flex={1}>
+                <Text color="gray.100" fontSize="md">
+                    Olá,
+                </Text>
+
+                <Heading color="gray.100" fontSize="md">
+                    João Andrade
+                </Heading>
+            </VStack>
+            
+            <TouchableOpacity>
+                <Icon
+                    as={MaterialIcons}
+                    name="logout"
+                    color="gray.200"
+                    size={7}
+                />
+            </TouchableOpacity>
+        </HStack>
+    )
+}
