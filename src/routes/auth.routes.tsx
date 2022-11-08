9c3b0c89410bc49694300d00bfa9534e0d5658d4
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-08 11:08:40 -0300

    feat: create navigation context

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -13,7 +13,9 @@ const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
 
 export function AuthRoutes() {
     return (
-        <Navigator>
+        <Navigator screenOptions={{
+            headerShown: false
+        }}>
             <Screen
                 name="signIn"
                 component={SignIn}
