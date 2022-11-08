5ba5ae5c6b78162a96b7820572fe8bade4151802
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-08 10:26:02 -0300

    feat: use variants in native base

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -41,6 +41,9 @@ export function SignIn() {
 
                 <Button title="Acessar" />
             </Center>
+
+
+            <Button title="Criar conta" variant="outline" />
         </VStack>
     );
 }
