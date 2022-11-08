0f10e958f3696d1208c5bd93a54cb28b1f5bc2cb
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-08 10:11:20 -0300

    feat: input component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,6 +3,7 @@ import { VStack, Image, Text, Center, Heading } from 'native-base';
 import BackgroundImg from '@assets/background.png';
 
 import LogoSvg from '@assets/logo.svg';
+import { Input } from '@components/Input';
 
 export function SignIn() {
     return (
@@ -26,6 +27,9 @@ export function SignIn() {
                 <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
                     Acesse sua conta
                 </Heading>
+
+                <Input placeholder='E-mail' />
+                <Input placeholder='Senha' />
             </Center>
         </VStack>
     );
