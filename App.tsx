fe4d9282003ec1b407402b7c86984dea42a0fdfa
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-08 09:56:44 -0300

    feat: background image

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import { View,  StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
 
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-        {fontsLoaded ? <View /> : <Loading />}
+        {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 };
