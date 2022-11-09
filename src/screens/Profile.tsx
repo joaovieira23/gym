82e3d56bcb9bf02fa6c53d4975a82dd0f9a13dec
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-09 12:26:19 -0300

    refactor: content profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -10,12 +10,12 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
 
-    const [photoIsLoading, setPhotoIsLoading] = useState(true);
+    const [photoIsLoading, setPhotoIsLoading] = useState(false);
 
     return (
         <VStack flex={1}>
             <ScreenHeader title="Perfil" />
-            <ScrollView>
+            <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
                     <Center mt={6} px={10}>
                 {
                         photoIsLoading ?
@@ -48,11 +48,8 @@ export function Profile() {
                         value='joaovictorvieira.23@hotmail.com'
                         isDisabled
                     />
-                    </Center>
 
-
-                    <VStack px={10} mt={12} mb={9}>
-                        <Heading color="gray.200" fontSize="md" mb={2}>
+                        <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}>
                             Alterar senha
                         </Heading>
 
@@ -78,7 +75,7 @@ export function Profile() {
                             title="Atualizar"
                             mt={4}
                         />
-                    </VStack>
+                    </Center>
             </ScrollView>
         </VStack>
     )
