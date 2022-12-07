e8dfd9452a73061295ed821654e82046666b0767
João Andrade <joaovictorvieira.23@hotmail.com>
2022-12-07 12:20:07 -0300

    feat: handle api errors

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,8 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
-import axios from 'axios';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 
@@ -12,7 +11,7 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { api } from "@services/api";
-import { Alert } from "react-native";
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -29,6 +28,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+
+  const toast = useToast();
   
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
@@ -48,8 +49,14 @@ export function SignUp() {
   
       console.log(response.data);
     } catch(error) {
-      if(axios.isAxiosError(error))
-      Alert.alert('Erro', error.response?.data.message);
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
     }
   }
 
