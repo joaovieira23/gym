4c8c82eca1856b69970d2774b5110e48f01b1f77
João Andrade <joaovictorvieira.23@hotmail.com>
2022-12-07 12:58:58 -0300

    feat: share states in context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { UserDTO } from "@dtos/UserDTO";
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 export type AuthContextDataProps = {
     user: UserDTO;
@@ -12,15 +12,16 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+
+    const [user, setUser] = useState({
+        id: '1',
+        name: 'João Andrade',
+        email: 'joaovictorvieira.23@hotmail.com',
+        avatar: 'joao.png'
+    });
+
     return (
-        <AuthContext.Provider value={{
-            user: {
-              id: '1',
-              name: 'João',
-              email: 'joaovictorvieira.23@hotmail.com',
-              avatar: 'joao.png'
-            }
-          }}>
+        <AuthContext.Provider value={{ user }}>
             {children}
           </AuthContext.Provider>
     );
