bf25c5ff2149c5014fe94ac9f39b269d4064d480
João Andrade <joaovictorvieira.23@hotmail.com>
2022-12-07 15:03:39 -0300

    feat: redirect logged in user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import { useTheme, Box } from 'native-base'
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from './app.routes';
+import { useAuth } from '@hooks/useAuth';
 
 
 export function Routes() {
     const { colors } = useTheme();
+    const { user } = useAuth();
 
     const theme = DefaultTheme;
     theme.colors.background = colors.gray[700];
@@ -15,8 +17,7 @@ export function Routes() {
     return (
         <Box flex={1} bg="gray.700">
             <NavigationContainer theme={theme}>
-                <AuthRoutes />
-                {/* <AppRoutes /> */}
+                {user.id ? <AppRoutes /> : <AuthRoutes />}
             </NavigationContainer>
         </Box>
     )
