894ba0d063998a1c67e1d15cb7940f63d765164d
João Andrade <joaovictorvieira.23@hotmail.com>
2022-12-08 12:10:52 -0300

    feat: attach token to requests

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageAuthTokenSave } from "@storage/storageAuthToken";
 import { storageUserGet, storageUserSave, storageUserRemove } from "@storage/storageUser";
 import { createContext, ReactNode, useEffect, useState } from "react";
 
@@ -13,7 +14,6 @@ export type AuthContextDataProps = {
 type AuthContextProviderProps = {
     children: ReactNode;
 };
-
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
@@ -21,12 +21,27 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     const [user, setUser] = useState<UserDTO>({} as UserDTO);
     const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+    async function storageUserAndToken(userData: UserDTO, token: string) {
+        try{
+            setIsLoadingUserStorageData(true);
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    
+            await storageUserSave(userData);
+            await storageAuthTokenSave(token);
+            setUser(userData);
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
     async function signIn(email: string, password: string) {
         try {
             const { data } = await api.post('/sessions', { email, password });
             if(data.user && data.token) {
-                setUser(data.user);
-                storageUserSave(data.user);
+                storageUserAndToken(data.user, data.token);
             }
         } catch(error) {
             throw error;
--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -2,6 +2,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { AUTH_TOKEN_STORAGE } from './storageConfig';
 
-export async function storageAuthTokenSabe(token: string) {
+export async function storageAuthTokenSave(token: string) {
     await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token);
 }
