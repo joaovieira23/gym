d27dcf93c2eeb9818ed3e9236ca45f8bc095fe1e
João Andrade <joaovictorvieira.23@hotmail.com>
2022-12-11 12:45:29 -0300

    chore: get invalid token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 import { AppError } from '@utils/AppError';
+import { storageAuthTokenGet } from '@storage/storageAuthToken';
 
 type SignOut = () => void;
 
@@ -13,11 +14,29 @@ const api = axios.create({
 }) as APIInstanceProps;
 
 api.registerInterceptTokenManager = signOut => {
-    const interceptTokenManager = api.interceptors.response.use(response => response, error => {
-        if(error.response && error.response.data) {
-            return Promise.reject(new AppError(error.response.data.message));
+    const interceptTokenManager = api.interceptors.response.use(response => response, async requestError => {
+
+        if(requestError?.response?.status === 401) {
+            if(requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+                const oldToken = await storageAuthTokenGet();
+
+                if(!oldToken) {
+                    signOut();
+                    return Promise.reject(new Error(requestError))
+                }
+            };
+
+            signOut();
+        }
+
+
+
+
+
+        if(requestError.response && requestError.response.data) {
+            return Promise.reject(new AppError(requestError.response.data.message));
         } else {
-            return Promise.reject(error);
+            return Promise.reject(requestError);
         }
     });
 
